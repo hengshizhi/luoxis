8b1e371c7ad13c7566dab9100a180d16c5f6d114
星姮十织 <87295882+hengshizhi@users.noreply.github.com>
2023-01-06 23:12:30 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import luoxis
-#示例代码
-class main(luoxis.luoxis):
-    def main(self):
-        self.When_the_message_is_this_send_this(so='傻逼',Send_this='你才是傻逼')
-if __name__ == '__main__':  
-    main = main()
-    main.go()
